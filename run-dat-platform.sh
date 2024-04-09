1d09e2a59ea859cf4d2185a152e0d3d241339a1f
Rijumone Choudhuri <mailmeonriju@gmail.com>
2024-04-09 19:43:51 +0530

    Updated run-dat-platform.sh

--- run-dat-platform.sh
+++ run-dat-platform.sh
@@ -20,13 +20,13 @@ done
 mkdir db-scripts
 
 # Step 4: Download an SQL file via curl and save it in db-scripts directory
-# curl -o db-scripts/database_dump.sql https://gist.githubusercontent.com/riju-dc/3aa36bbe655eb0eb0bc5f25cf340de2e/raw/52d630cfa526ba6f3d294edad60eb995db79ded1/001-create-db-seed.sql
+curl -o db-scripts/001-create-db-seed.sql https://raw.githubusercontent.com/dat-labs/dat-main/main/db-scripts/001-create-db-seed.sql
 
 # Step 5: Download a docker-compose file via curl
-# curl -O https://gist.githubusercontent.com/riju-dc/e9805fc1fc8be0f2b141a52006560630/raw/a60641ac022678aa3fc6d1e72e7532283eb89d3b/dat-docker-compose.yml
+curl -O https://raw.githubusercontent.com/dat-labs/dat-main/main/docker-compose.yml
 
 # Step 6: Run docker compose build
-docker compose build
+docker compose build --no-cache
 
 # Step 7: Run docker compose up
 docker compose up
