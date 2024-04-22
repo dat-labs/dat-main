d465cce9c9dd76dc3d6fc216d843821d462f39ad
Rijumone Choudhuri <mailmeonriju@gmail.com>
2024-04-23 00:00:58 +0530

    Added: - docker-compose-dev.yml - samples/minio_test.py

---
+++ samples/minio_test.py
@@ -0,0 +1,42 @@
+# file_uploader.py MinIO Python SDK example
+from minio import Minio
+from minio.error import S3Error
+
+def main():
+    # Create a client with the MinIO server playground, its access key
+    # and secret key.
+    client = Minio("object-storage:9000",
+        access_key="los_user",
+        secret_key="los_password",
+        secure=False,
+    )
+
+    # The file to upload, change this path if needed
+    source_file = "app/routers/minio_test.py"
+
+    # The destination bucket and filename on the MinIO server
+    bucket_name = "python-test-bucket"
+    destination_file = "my-test-file.txt"
+
+    # Make the bucket if it doesn't exist.
+    found = client.bucket_exists(bucket_name)
+    if not found:
+        client.make_bucket(bucket_name)
+        print("Created bucket", bucket_name)
+    else:
+        print("Bucket", bucket_name, "already exists")
+
+    # Upload the file, renaming it in the process
+    client.fput_object(
+        bucket_name, destination_file, source_file,
+    )
+    print(
+        source_file, "successfully uploaded as object",
+        destination_file, "to bucket", bucket_name,
+    )
+
+if __name__ == "__main__":
+    try:
+        main()
+    except S3Error as exc:
+        print("error occurred.", exc)
